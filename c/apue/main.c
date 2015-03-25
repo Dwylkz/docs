7c1d30ad684128dec9b3a4abc40f9ba35be258f1
Dwylkz <xellosgeteternal@gmail.com>
2015-03-25 23:20:26 +0800

    add sicp

--- c/apue/main.c
+++ c/apue/main.c
@@ -1,10 +1,11 @@
+#include <apue.h>
+
 #include <assert.h>
-#include <stdio.h>
 #include <dirent.h>
 
 #include <dlib_comm.h>
 
-int ls(int argc, char** argv)
+static int ls(int argc, char** argv)
 {
   char* dir = ".";
   if (argc == 2) dir = argv[1];
@@ -20,10 +21,21 @@ int ls(int argc, char** argv)
   return 0;
 }
 
+// unbuffered io
+static int ubio(int argc, char** argv)
+{
+  char buf[BUFSIZ];
+  int nread;
+  while ((nread = read(STDIN_FILENO, buf, BUFSIZ)) > 0)
+    assert(write(STDOUT_FILENO, buf, nread) >= 0);
+  return 0;
+}
+
 int main(int argc, char** argv)
 {
   const dlib_cmd_t cmds[] = {
     DLIB_CMD_DEFINE(ls, "[<dir>]"),
+    DLIB_CMD_DEFINE(ubio, ""),
     DLIB_CMD_NULL
   };
   return dlib_subcmd(argc, argv, cmds);
